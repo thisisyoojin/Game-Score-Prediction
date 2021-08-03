c6012ca3f41ba3324745b26d6642fcc5a45fd5ec
thisisyoojin <thisisyoojin@gmail.com>
2021-08-03 13:43:23 +0100

    Amends a notebook

--- steam/validation/__init__.py
+++ steam/validation/__init__.py
@@ -1,3 +1,3 @@
 from .base import Base
 from .models import RidgeCV, LassoCV, KnnCV, SvrCV, RandomForestCV, ExtraTreesRegressor
-from .best_model import set_baseline, get_best_models, plot_train_val, find_winner
+from .best_model import set_baseline, get_best_models, plot_train_val
